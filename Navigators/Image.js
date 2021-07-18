39e68efe2e34993f67661158d7baabfbeae30b1e
Lokesh01 <lokeshnegi649@gmail.com>
2021-07-18 13:30:37 +0530

    replaced api key with env variable

--- Navigators/Image.js
+++ Navigators/Image.js
@@ -1,7 +1,7 @@
 const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: "ae28d77ec6ac4e719be4fc9454b0702f",
+  apiKey: process.env.API_CLARIFAI,
 });
 
 const handleAPI = (req, res) => {
